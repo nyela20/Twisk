581f4db9fc1349cb6732a2777243d501361dc2dc
aboubacar <abouchekou@gmail.com>
2021-01-29 10:45:20 +0100

    1er depot twisk

---
+++ src/twisk/Main.java
@@ -0,0 +1,22 @@
+package twisk;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Projet twisk");
+        BorderPane root = new BorderPane();
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
