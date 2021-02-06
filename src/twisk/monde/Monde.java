f5f8c4d8857cdb3fea194e58e769c2658e0a00da
ralanton1u <ny-elanirina-n.ralantonisainana1@etu.univ-lorraine.fr>
2021-02-06 16:09:55 +0100

    depot

--- src/twisk/monde/Monde.java
+++ src/twisk/monde/Monde.java
@@ -6,10 +6,12 @@ public class Monde /*implements Iterable*/ {
     private Etape SasEntree;
     //private GestionnaireEtapes GestionnaireDesEtapes;
 
+
     public Monde(){
         Etape SasEntree = new Activite("SasEntree");
         Etape SasSortie = new Activite("SasSortie");
     }
+
 
     void aCommeEntree(Etape ... etapes){
 
