90098011fb175f761f75605334466c9f76b301ab
aboubacar <abouchekou@gmail.com>
2021-03-13 14:53:05 +0100

    TP5 terminé; Point a revoir:
    -Ajout des commentaires et simplification de la lecture;
    -enlever les toStirng des tests

--- src/twisk/outils/KitC.java
+++ src/twisk/outils/KitC.java
@@ -2,6 +2,11 @@ package twisk.outils;
 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class KitC {
 
 
@@ -12,22 +17,20 @@ public class KitC {
         FileWriter flot;
         BufferedWriter flotFiltre;
 
-        //creation du dossier et recopier des fichers
-        File dossier = new File("/tmp/twisk");
-        dossier.mkdir();
 
-
-      /*  try {
-            if (dossier.exists()) {
-                String dest1 = "/tmp/twisk/def.h";
-                String src1 = getClass().getResource("/def.h").toExternalForm();
-                String src2 = getClass().getResource("/programmeC.o").toExternalForm();
-                Files.copy(Paths.get(src1), Paths.get(dest1));
-
+        try {
+            // création du répertoire twisk sous /tmp. Ne déclenche pas d’erreur si le répertoire existe déjà
+            Path directories = Files.createDirectories(Paths.get("/tmp/twisk"));
+            // copie des deux fichiers programmeC.o et def.h depuis le projet sous /tmp/twisk
+            String[] liste = {"programmeC.o", "def.h"};
+            for (String nom : liste) {
+                Path source = Paths.get(getClass().getResource("/codeC/" + nom).getPath());
+                Path newdir = Paths.get("/tmp/twisk/");
+                Files.copy(source, newdir.resolve(source.getFileName()), REPLACE_EXISTING);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
         File chemin = new File("/tmp/twisk/client.c");
         try {
@@ -36,7 +39,7 @@ public class KitC {
             flotFiltre.write(codeC);
             flotFiltre.close();
         } catch (IOException e) {
-            System.out.println("Impossible d'ecrire le code C");
+            e.printStackTrace();
         }
     }
 
