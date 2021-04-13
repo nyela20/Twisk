0305ac768ac4750a8e1a490059c1f756fcc7fc78
aboubacar <abouchekou@gmail.com>
2021-04-13 16:30:10 +0200

    TP7 FINI ouf

--- src/twisk/test/MondeTest.java
+++ src/twisk/test/MondeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import twisk.monde.*;
 import twisk.outils.FabriqueNumero;
+import twisk.simulation.Simulation;
 
 import java.sql.SQLOutput;
 
@@ -87,8 +88,28 @@ class MondeTest {
 
     @Test
     void toC(){
-        //multiplier
 
+        Monde monde = new Monde();
+
+        Activite zoo = new Activite("balade_au_zoo", 3, 1);
+        Guichet guichettob = new Guichet("acces_au_toboggan", 2);
+        Activite tob = new ActiviteRestreinte("toboggan", 2, 1);
+        Guichet guichetpiscne = new Guichet("acces_a_la_piscine",2);
+        Activite piscine = new ActiviteRestreinte("piscine",3,1);
+
+        monde.ajouter(zoo, guichettob,tob,guichetpiscne,piscine);
+
+        zoo.ajouterSuccesseur(guichettob);
+        guichettob.ajouterSuccesseur(tob);
+        tob.ajouterSuccesseur(guichetpiscne);
+        guichetpiscne.ajouterSuccesseur(piscine);
+
+        monde.aCommeEntree(zoo);
+        monde.aCommeSortie(piscine);
+
+        Simulation s = new Simulation();
+        s.setNbClients(5);
+        s.simuler(monde);
     }
 
 
